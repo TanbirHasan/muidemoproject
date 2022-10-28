428101457b32223cacb8efc81c67a7c2f79eab53
unknown <mdtanbir576@gmail.com>
2022-10-28 11:07:16 +0600

    maincomp

---
+++ src/components/Maincomp.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Maincomp = () => {
+  return (
+    <div>
+      <div>
+        <h4>My name and information</h4>
+      </div>
+      <div>
+        <ul>
+          <li>name</li>
+          <li>info</li>
+          <li>address</li>
+          <li>city</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Maincomp;
